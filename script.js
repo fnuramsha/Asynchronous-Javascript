2982711b1200945579c152b169fe81924f01a4c3
Ramsha <fnuramsha@gmail.com>
2024-04-16 16:54:21 +0200

    implemented Country data basic code

--- script.js
+++ script.js
@@ -4,12 +4,33 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  // request.open("GET", "https://restcountries.eu/rest/v2/name/portugal");
+  request.send();
 
-const request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/name/portugal");
-request.send();
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    // Now add cards
+    const html = `<article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      data.population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+    <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
+  </div>
+  </article>`;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+  });
+};
 
-request.addEventListener("load", function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
-});
+getCountryData("portugal");
+getCountryData("germany");
+getCountryData("usa");
